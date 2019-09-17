3bf1593319df51008be7000b47007a217286ff38
jhm-usc <42620415+jhm-usc@users.noreply.github.com>
2019-09-17 15:15:41 -0400

    Add files via upload

---
+++ driver.py
@@ -0,0 +1,8 @@
+#!/bin/python
+
+import interface.py
+
+interface = Interface()
+interface.open
+interface.connect
+interface.restart
---
+++ interface.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+import serial
+import time
+
+class Interface:
+    def __init__(self):
+        self.connector
+    def open(self):
+        #self.connection.open()
+        kDelay = 0.0125
+        time.sleep(kDelay)
+    def connect(self):
+        connection = serial.Serial('/dev/ttyUSB0', baudrate=115200)
+    def start(self):
+        connection.write(chr(128))
+    def restart(self):
+        connection.write(chr(7))
+    def stop(self):
+        connection.write(chr(173))
+    def poweroff(self):
+        connection.write(chr(133))
+    def read(self):
+        connection.read(1)
+        
