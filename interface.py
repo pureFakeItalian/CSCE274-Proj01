085356a7125945311baf1a2f438d165c6fea2daa
pureFakeItalian <55857912+pureFakeItalian@users.noreply.github.com>
2019-09-28 13:04:51 -0400

    Code for the project, finished on friday
    This is Chris, by the way. I also made this account on friday.

--- interface.py
+++ interface.py
@@ -1,42 +1,33 @@
-import serial
-import time
+#Written by Christopher Brooks, Jackson Matthews, and Zachary Lynch
+#Copyright 2019
+#Only permitted for use in projects written by Christopher Brooks, Jackson Matthews and/or Zachary Lynch
 
+from serial import Serial
+from time import sleep
 class Interface:
-    def __init__(self):
-        self.connector
-    def open(self):
-        #self.connection.open()
-        kDelay = 0.0125
-        time.sleep(kDelay)
-    def connect(self):
-        connection = serial.Serial('/dev/ttyUSB0', baudrate=115200)
-    def start(self):
-        connection.write(chr(128))
-    def restart(self):
-        connection.write(chr(7))
-    def stop(self):
-        connection.write(chr(173))
-    def poweroff(self):
-        connection.write(chr(133))
-    def read(self):
-        connection.read(1)
-    def passiveMode(self):
-        # Start command changes mode to passive
-        # Not sure if there's strictly a passive opcode
-        connection.write(chr(128)) 
-    def safeMode(self):
-        connection.write(chr(131)) 
-    def drive(self):
-        connection.write(chr(137))
-        # [Velocity high byte] [Velocity low byte] [radius high byte] 
-        # [radius low byte]
-        # P.13 - OI Manual
-        #
-        # Test drive
-        # Should drive in reverse @ -200 mm/s & turn @ radius of 500 mm
-        # need to set values of movement as parameters of function after testing for functionality
-        connection.write(chr(255))
-        connection.write(chr(56))
-        connection.write(chr(1))
-        connection.write(chr(244))
-        
+
+	#Interface serves as a manager for the serial connection between the Raspberry Pi and the Roomba.
+	#Each command in the interface is sending a series of bytes to the Roomba.
+	#Each command is defined beforehand and can be called from whereever the interface is imported.
+
+	#Command Definitions
+	def __init__(self):
+		self.connection = Serial('/dev/ttyUSB0', baudrate=115200)	#serial connection to Roomba
+
+	def powerOff(self):
+		self.connection.write(chr(133))
+
+	def readBytes(n, self):	#returns an array of n bytes
+		ret = self.connection.read(n)
+		return ret
+
+	def write(data, self):
+		self.connection.write(data)
+
+	def close(self):
+		self.connection.close()
+
+#======TEST COMMANDS FOR VERIFYING A CONNECTION======#
+#interface = Interface()
+#interface.powerOff()
+#interface.close()
